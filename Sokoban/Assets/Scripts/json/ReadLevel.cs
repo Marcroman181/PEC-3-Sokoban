695b12ce2a992395a373556c5120d09b16af0cb0
Marc Roman <marqui7@gmail.com>
2022-04-26 20:13:07 +0200

    fix save and read json

--- Sokoban/Assets/Scripts/json/ReadLevel.cs
+++ Sokoban/Assets/Scripts/json/ReadLevel.cs
@@ -7,26 +7,21 @@ using UnityEngine;
 public class ReadLevel
 {
 
-    private static string PATH_CUSTOM_FILE = "Assets/Resources/Levels/";
-    private static string PATH_FILE = "Assets/Resources/StandardLevels/";
+    private static string PATH_FILE = "StandardLevels/";
 
     private static string FILE_EXTENSION = ".json";
     private static string FILE_META_EXTENSION = ".json.meta";
 
     public static List<Box> Read()
     {
-        string path = PATH_FILE + GameConfiguration.levelName + FILE_EXTENSION;
-        if (System.IO.File.Exists(path))
-        {
-            string json = File.ReadAllText(path);
-            Level Level = JsonUtility.FromJson<Level>(json);
-            return Level.BoxList;
-        }
-        return null;
+       string path = PATH_FILE + GameConfiguration.levelName;
+       TextAsset txtAsset = (TextAsset)Resources.Load(path, typeof(TextAsset));
+       Level Level = JsonUtility.FromJson<Level>(txtAsset.text);
+       return Level.BoxList;
     }
     public static List<Box> ReadCustom()
     {
-        string path = PATH_CUSTOM_FILE + GameConfiguration.levelName + FILE_EXTENSION;
+        string path = Application.dataPath + "/" + GameConfiguration.levelName + FILE_EXTENSION;
         if (System.IO.File.Exists(path))
         {
             string json = File.ReadAllText(path);
@@ -39,12 +34,14 @@ public class ReadLevel
     public static List<string> AllCustomLevels()
     {
         List<string> files = new List<string>();
-        foreach (string file in System.IO.Directory.GetFiles(PATH_CUSTOM_FILE)) {
+        string path = Application.dataPath + "/";
+        foreach (string file in System.IO.Directory.GetFiles(path))
+        {
             if (file.Contains(FILE_EXTENSION) && !file.Contains(FILE_META_EXTENSION))
             {
-                files.Add(file.Replace(PATH_CUSTOM_FILE, "").Split('.')[0]);
+                files.Add(file.Replace(path, "").Split('.')[0]);
             }
-        };
+        }
         return files;
     }
 }
--- Sokoban/Assets/Scripts/json/SaveLevel.cs
+++ Sokoban/Assets/Scripts/json/SaveLevel.cs
@@ -6,13 +6,12 @@ using UnityEngine;
 
 public class SaveLevel
 {
-    private static string PATH_FILE = "Assets/Resources/Levels/";
 
     private static string FILE_EXTENSION = ".json";
 
     public static bool Save(string nameLevel, Level level)
     {
-        string path = PATH_FILE + nameLevel + FILE_EXTENSION;
+        string path = Application.dataPath + "/" + nameLevel + FILE_EXTENSION;
         if (!System.IO.File.Exists(path))
         {
             File.WriteAllText(path, JsonUtility.ToJson(level));
